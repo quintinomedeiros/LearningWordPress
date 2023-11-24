abcaad742d0e635a4ad7fda1561b899b027f50c8
quintinomedeiros <quintinomedeiros@yaho.com.br>
2023-11-24 18:11:33 -0300

    LearningWordPress Init

---
+++ footer.php
@@ -0,0 +1,13 @@
+        
+        </main>
+        <!-- /Conteúdo principal -->
+
+        <!-- Rodapé -->
+        <footer class="site-footer">
+            <p><?php bloginfo('name'); ?> &copy - <?php echo date("Y");?></p>
+        </footer>
+        <!-- /Rodapé -->
+
+    <?php wp_footer(); ?>
+</body>
+</html>
